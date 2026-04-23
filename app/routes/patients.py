5878b9af312fc678ab3eccec7bb704f3e619b81e
aarnavanoop <aarnavanoop@gmail.com>
2026-04-24 03:26:15 +1000

    feat: implement GET /patients endpoint
    - Added paginated and filtered GET /patients route
     using FastAPI Query parameters.
    
     - Verified async database sessions and verified
      JSON response formatting.

--- app/routes/patients.py
+++ app/routes/patients.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
+from typing import Optional
+from datetime import date
 import logging
 from app.db.deps import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -15,13 +17,26 @@ router = APIRouter(
 )
 
 @router.get("/")
-async def get_patients(db: AsyncSession = Depends(get_db)):
+async def get_patients(
+    db: AsyncSession = Depends(get_db),
+    page: int = Query(1, ge=1, description = "Page number (starts at 1)"),
+    size: int = Query(10, ge=1, le=100, description= "Number of records per page"),
+    anomaly_flag: Optional[bool] = Query(None, description="Filter by anomaly presence"),
+    ):
     """
     Returns a paginated list of patients
     """
 
-    logger.info("Fetched patient list")
-    stmt = select(Patient).limit(10)
+    logger.info(f"Fetching patient list -> page: {page}, size: {size}")
+
+    stmt = select(Patient)
+    if anomaly_flag is not None:
+        stmt = stmt.where(Patient.anomaly_flag == anomaly_flag)
+
+    offset_value = (page - 1) * size
+    stmt = stmt.offset(offset_value).limit(size)
+
     result = await db.execute(stmt)
     patients = result.scalars().all()
+        
     return patients
